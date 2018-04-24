65fdaca3229cd38b11234cc12c0d3f3087ac1f24
Harkishen-Singh <harkishensingh@hotmail.com>
2018-04-24 13:19:30 +0530

    new update from today

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.BlogListView.as_view(), name='homepage'),    
+    path('', views.BlogListView.as_view(), name='homepage'),
+    path('post', views.BlogListView.as_view(), name='allPosts'),
+    path('post/<int:pk>', views.BlogDetailedView.as_view(), name='detailedBlog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
 from .models import Post
@@ -7,3 +7,8 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class BlogDetailedView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'anything'
