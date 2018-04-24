31d11636101dd23c2d3d80af72a7b697e2e5ea90
Harkishen-Singh <harkishensingh@hotmail.com>
2018-04-24 14:39:52 +0530

    final commits

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.BlogListView.as_view(), name='homepage'),
     path('post', views.BlogListView.as_view(), name='allPosts'),
     path('post/<int:pk>', views.BlogDetailedView.as_view(), name='detailedBlog'),
-    path('post/new/', views.BlogCreateView.as_view(), name='post_new',)
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 from .models import Post
@@ -17,3 +18,14 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
